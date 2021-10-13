a3b416709dd226d6a5812ffcfb7b32cfb0967406
vradionovskii <vradionovskii@yandex.ru>
2021-10-13 22:01:37 +0300

    some more fixes

--- components/Modal.js
+++ components/Modal.js
@@ -10,7 +10,7 @@ import Tag from "./Tag";
 
 const Modal = ({ onClose, show }) => {
   const [salary, setSalary] = useState("");
-  const [number, setNumber] = useState("");
+  const [finalNumber, setFinalNumber] = useState("");
   const [errorState, setErrorState] = useState(false);
   const [isPayment, setIsPayment] = useState(true);
 
@@ -49,7 +49,7 @@ const Modal = ({ onClose, show }) => {
         <div className="fixed inset-0 flex items-center justify-center overflow-y-scroll">
           <ModalBackground onClose={onClose} />
           <div className=" absolute top-0 md:py-[7.5rem] pointer-events-none min-h-screen md:min-h-0">
-            <div className="pointer-events-auto relative flex flex-col  w-full h-full px-4 pt-8 pb-4 md:py-8 bg-white md:max-w-[28.125rem] lg:max-w-[34.5rem]  md:px-8 md:rounded-[1.875rem]">
+            <div className="pointer-events-auto relative flex flex-col  w-full md:min-h-0 min-h-screen px-4 pt-8 pb-4 md:py-8 bg-white md:max-w-[28.125rem] lg:max-w-[34.5rem]  md:px-8 md:rounded-[1.875rem]">
               <CloseButton onClick={onClose} />
               <IntroText />
               <Input
@@ -59,12 +59,12 @@ const Modal = ({ onClose, show }) => {
                 onBlur={() => validate()}
               />
               <button
-                onClick={() => setNumber(salaryOnlyNumbers())}
+                onClick={() => setFinalNumber(salaryOnlyNumbers())}
                 className="py-4 mr-auto text-sm font-medium transition-colors duration-200 text-theme-red active:text-theme-red hover:text-theme-red-hover"
               >
                 Рассчитать
               </button>
-              {number && <CheckBoxes salary={number} />}
+              {finalNumber && <CheckBoxes salary={finalNumber} />}
               <div className="items-center md:flex md:pt-6">
                 <p className="pb-6 pt-2 md:py-0 text-sm font-medium pr-[4.75rem]">
                   Что уменьшаем?
--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,15 @@ import Modal from "../components/Modal";
 
 export default function Index() {
   const [show, setShow] = useState(false);
-  function toggleModal(isOpen) {
-    document.body.style.overflow = isOpen ? "initial" : "hidden";
-    setShow(!isOpen);
+  if (process.browser) {
+    document.body.style.overflow = show ? "hidden" : "initial";
   }
   return (
     <section className="flex items-center justify-center w-screen h-screen main-background-gradient">
       <Button onClick={() => setShow(!show)} stroke>
         Налоговый вычет
       </Button>
-      <Modal title="My Modal" onClose={() => toggleModal(false)} show={show}>
-        <p>This is modal body</p>
-      </Modal>
+      <Modal onClose={() => setShow(false)} show={show} />
     </section>
   );
 }
